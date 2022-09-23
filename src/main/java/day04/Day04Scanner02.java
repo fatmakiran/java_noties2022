493cc48b560850d846866ddd8abf116f64fd510f
Fatma Kiran <ftmkrntest@gmail.com>
2022-09-23 10:07:17 -0400

    Create a Scanner Class object

---
+++ src/main/java/day04/Day04Scanner02.java
@@ -0,0 +1,25 @@
+package day04;
+
+import java.util.Scanner;
+
+public class Day04Scanner02 {
+   /*
+   ask user to enter  the width  and the length  of a rectangle then print the area and perimeter n te cinsole
+
+
+    */
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the width of the rectangle");
+        double width = input.nextDouble();
+        System.out.println("Enter the length of the rectangle");
+        double length = input.nextDouble();
+        System.out.println("Area is :"+width*length);
+        System.out.println("Perimeter is:"+2*(width+length));
+
+
+
+    }
+}
---
+++ src/main/java/day04/Day04Scanner03.java
@@ -0,0 +1,28 @@
+package day04;
+
+import java.util.Scanner;
+
+public class Day04Scanner03 {
+    /*
+    Ask user to enter the width ,length  and the height of a rectangular prism then find the valume
+    volume = width * length * height
+     */
+
+
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter width");
+        double w= input.nextDouble();
+
+        System.out.println("Enter the length");
+        double l = input.nextDouble();
+
+        System.out.println("Enter the height");
+        double h = input.nextDouble();
+
+        System.out.println("Volume is : "+ w*l*h);
+
+    }
+}
